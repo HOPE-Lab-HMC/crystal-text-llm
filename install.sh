4a8713fc92f021b6123235ba9b44afc6b799c7c4
Andy Xu <axeman775@gmail.com>
2024-11-01 15:13:59 -0700

    initial testing of slurm SUs

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-conda create -n crystal-llm
+conda create -n crystal-llm python=3.9
 conda activate crystal-llm
+conda install pip
 
 pip install torch torchvision torchaudio
 pip install accelerate peft transformers sentencepiece datasets
